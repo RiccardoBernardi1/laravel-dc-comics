0968e2c9e1bc89d0d9f704bbe9ab49db8e644a82
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-01-19 15:22:58 +0100

    Crea la homepage con la lista di fumetti

--- app/Http/Controllers/Admin/ResourceController.php
+++ app/Http/Controllers/Admin/ResourceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ResourceController extends Controller
@@ -14,7 +15,8 @@ class ResourceController extends Controller
      */
     public function index()
     {
-        return view('homepage');
+        $comics=Comic::all();
+        return view('homepage',compact('comics'));
     }
 
     /**
